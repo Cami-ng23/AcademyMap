68dc6f3a1bb9e05fd23e2cc65efa8a79dfc3d029
Cami-ng23 <deiberdiaz@liceovvh.cl>
2026-07-23 23:55:01 -0400

    casi terminao, falta mapa

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+
 """Expone el modelo y el repositorio de liceos como `models.liceo`."""
-from ..AcademyMap.models import liceo  # noqa: F401
+from . import liceo  # noqa: F401
 from .liceo import Liceo  # noqa: F401
